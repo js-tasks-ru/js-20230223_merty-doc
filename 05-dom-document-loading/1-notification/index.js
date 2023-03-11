186a8ab9f5a602b07c8e8dcb748b29d375a731c7
Konstantin D <merty.doc@gmail.com>
2023-03-11 16:37:43 +0300

    notification & sortable-table components

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,49 @@
 export default class NotificationMessage {
+    constructor(message = '', { duration = 2000, type = 'success' } = {}) {
+        this.message = message;
+        this.duration = duration;
+        this.type = type;
 
+        this.render();
+    }
+
+    get template() {
+        return `
+        <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+            <div class="timer"></div>
+            <div class="inner-wrapper">
+                <div class="notification-header">${this.type}</div>
+                <div class="notification-body">${this.message}</div>
+            </div>
+        </div>
+        `;
+    }
+
+    render() {
+        const wrapper = document.createElement('div');
+        wrapper.innerHTML = this.template;
+        this.element = wrapper.firstElementChild;
+    }
+
+    show(container = document.body) {
+        const prevNotificationElem = container.querySelector('.notification');
+        if (prevNotificationElem) prevNotificationElem.remove();
+
+        container.append(this.element);
+
+        setTimeout(() => {
+            this.remove();
+        }, this.duration);
+    }
+
+    remove() {
+        if (this.element) {
+            this.element.remove();
+        }
+    }
+
+    destroy() {
+        this.remove();
+        this.element = null;
+    }
 }
